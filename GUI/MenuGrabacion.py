4dec30cd3d6071fcddba980889ae5068e81fdf22
CarlosAHzBt <carlos.hb@culiacan.tecnm.mx>
2024-03-05 14:49:04 -0700

    Logica para grabar segmentos de 15segs

--- GUI/MenuGrabacion.py
+++ GUI/MenuGrabacion.py
@@ -15,6 +15,10 @@ class MenuGrabacion:
         self.grabador = GrabadorBag(15)
         self.carpeta_destino = None
 
+        # Añade un manejador de evento para el cierre de la ventana
+        self.master.protocol("WM_DELETE_WINDOW", self.on_cerrar)
+        
+
     def setup_gui(self):
         self.label_carpeta = tk.Label(self.master, text="Carpeta de destino no seleccionada")
         self.label_carpeta.pack(pady=5)
@@ -25,6 +29,11 @@ class MenuGrabacion:
         boton_grabar = tk.Button(self.master, text="Iniciar Grabación", command=self.iniciar_grabacion)
         boton_grabar.pack(pady=5)
 
+        boton_detener_grabacion = tk.Button(self.master, text="Detener Grabación", command=self.detener_grabacion)
+        boton_detener_grabacion.pack(pady=5)
+        # Asumiendo que el resto de la inicialización de la clase permanece igual
+        boton_cerrar_pipeline = tk.Button(self.master, text="Cerrar Pipeline", command=self.cerrar_pipeline)
+        boton_cerrar_pipeline.pack(pady=5)
     def seleccionar_carpeta(self):
         if sys.platform == 'win32':
             # Comando para abrir el diálogo de selección de carpeta en Windows
@@ -46,8 +55,24 @@ class MenuGrabacion:
         if not self.carpeta_destino:
             messagebox.showerror("Error", "Por favor selecciona una carpeta de destino.")
             return
-        self.grabador.ejecutar_grabacion(self.carpeta_destino)
+        self.hilo_grabacion = threading.Thread(target=self.grabador.ejecutar_grabacion, args=(self.carpeta_destino,), daemon=True)
+        self.hilo_grabacion.start()
         messagebox.showinfo("Grabación Iniciada", "La grabación ha comenzado.")
-        self.master.destroy()
 
-    
+    def detener_grabacion(self):
+        if not self.carpeta_destino:  # Si no se ha seleccionado carpeta o iniciado la grabación, no hacer nada
+            return
+        self.grabador.detener_grabacion()  # Configura `self.grabando` en `False` para detener la grabación
+        self.hilo_grabacion.join()  # Espera a que el hilo de grabación termine
+        messagebox.showinfo("Grabación Detenida", "La grabación ha sido detenida correctamente.")
+
+    def on_cerrar(self):
+        """Manejador para el evento de cierre de la ventana."""
+        if messagebox.askokcancel("Salir", "¿Estás seguro de que quieres salir?"):
+            self.detener_grabacion()  # Asegura la detención adecuada de la grabación
+            self.grabador.cerrar_pipeline()  # Asegura que el pipeline se cierra correctamente
+            self.master.destroy()  # Cierra la ventana
+            sys.exit()  # Cierra toda la aplicación
+    def cerrar_pipeline(self):
+        self.grabador.cerrar_pipeline()
+        messagebox.showinfo("Pipeline Cerrado", "El pipeline se ha cerrado correctamente.")
--- LogicaCapturaDatos/GrabadorBag.py
+++ LogicaCapturaDatos/GrabadorBag.py
@@ -2,52 +2,65 @@ import pyrealsense2 as rs
 import time
 import datetime
 import os
+import threading
 
 class GrabadorBag:
     def __init__(self, duracion=15):
         self.config = rs.config()
         self.duracion = duracion
         self.configurar_streams()
+        self.grabando = False
+        self.finalizado = threading.Event()  # Para sincronizar la finalización de la grabación
+        self.pipeline_activa = False  # Añadir esta línea
+
 
     def configurar_streams(self):
-        """Configura los streams de profundidad y color."""
         self.config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
         self.config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30)
 
-    def generar_nombre_archivo_en_carpeta_seleccionada(self,):
-        """Genera un nombre de archivo basado en la fecha y hora actual."""
-        #guardar el archivo en la carpeta seleccionada
-        return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".bag"
-         
+    def generar_nombre_archivo_en_carpeta_seleccionada(self, carpeta_destino):
+        nombre_archivo = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".bag"
+        return os.path.join(carpeta_destino, nombre_archivo)
 
-    def iniciar_pipeline(self,carpeta_destino):
-        """Inicia el pipeline de grabación con la configuración dada."""
-        self.nombre_archivo = self.generar_nombre_archivo_en_carpeta_seleccionada()
-        # guardar el archivo en la carpeta destino con el nombre asignado
-        self.nombre_archivo = carpeta_destino + self.nombre_archivo
-        self.config.enable_record_to_file(self.nombre_archivo)
+    def grabar_segmento(self, carpeta_destino):
+        nombre_archivo = self.generar_nombre_archivo_en_carpeta_seleccionada(carpeta_destino)
+        self.config.enable_record_to_file(nombre_archivo)
         self.pipeline = rs.pipeline()
         self.pipeline.start(self.config)
+        self.pipeline_activa = True  # Establecer a True después de llamar a start()
+        print(f"Grabación iniciada, guardando en {nombre_archivo}.")
 
+        inicio = time.time()
+        while time.time() - inicio < self.duracion and self.grabando:
+            time.sleep(0.01)
 
-    def grabar(self):
-        """Realiza la grabación durante un tiempo especificado."""
-        print(f"Grabación iniciada, guardando en {self.nombre_archivo}.")
-        time.sleep(self.duracion)
+        if self.pipeline_activa:  # Verificar antes de llamar a stop()
+            self.pipeline.stop()
+            self.pipeline_activa = False  # Establecer a False después de detener la pipeline
         print("Grabación detenida.")
 
-    def detener_pipeline(self):
-        """Detiene el pipeline."""
-        self.pipeline.stop()
-
-    def guardar_bag(self, carpeta_destino):
-        """Guarda el archivo .bag en la carpeta de destino."""
-        self.nombre_archivo = os.path.join(carpeta_destino, self.nombre_archivo)
-        os.rename(self.nombre_archivo, self.nombre_archivo)
-
     def ejecutar_grabacion(self, carpeta_destino):
-        """Método principal para ejecutar la grabación."""
-        self.carpeta_destino=carpeta_destino
-        self.iniciar_pipeline(carpeta_destino)
-        self.grabar()
-        self.detener_pipeline()
+        self.grabando = True
+        self.finalizado.clear()  # Prepara el evento para una nueva sesión de grabación
+        while self.grabando:
+            self.grabar_segmento(carpeta_destino)
+        self.finalizado.set()  # Indica que la grabación ha finalizado
+
+    def detener_grabacion(self):
+        self.grabando = False
+        # Asumiendo que self.pipeline puede haber sido inicializado pero no necesariamente empezado
+        try:
+            if self.pipeline_activa:
+                self.pipeline.stop()
+                print("Pipeline detenido correctamente.")
+        except Exception as e:
+            print(f"Error al detener el pipeline: {e}")
+        finally:
+            self.pipeline_activa = False  # Asegura que el estado refleje que el pipeline ya no está activo
+    
+        self.finalizado.set()  # Indica la finalización para cualquier espera pendiente
+    def cerrar_pipeline(self):
+        if self.pipeline_activa:
+            self.pipeline.stop()
+            self.pipeline_activa = False
+            print("Pipeline cerrado correctamente.")
